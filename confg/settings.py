b196739f7ae37858b877429f96d522b999fdd531
YasamanAlinia <yasamanalinia.dev@gmail.com>
2025-12-12 14:41:07 +0330

    Add initial apps accounts and library, update settings and urls

--- confg/settings.py
+++ confg/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from .local_setting import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'library.apps.LibraryConfig',
 ]
 
 MIDDLEWARE = [
